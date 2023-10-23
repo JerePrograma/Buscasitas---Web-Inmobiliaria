11d33d3962ab7bd54b7c31f2e08d84dde280541f
Jeremias Rivelli <jeremias.j.riv@gmail.com>
2023-10-23 17:43:26 -0300

    Proyecto Equipo A inciado

---
+++ ProyectoFinal/src/main/java/com/proyectofinal/demo/ProyectoFinalEquipoAApplication.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalEquipoAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalEquipoAApplication.class, args);
+	}
+
+}
---
+++ ProyectoFinal/src/test/java/com/proyectofinal/demo/ProyectoFinalEquipoAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalEquipoAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
