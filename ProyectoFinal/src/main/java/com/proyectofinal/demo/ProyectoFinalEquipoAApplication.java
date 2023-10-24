b2d2183f81df59ed817850ca6200f9bb3a2e7279
Jeremias Rivelli <jeremias.j.riv@gmail.com>
2023-10-23 21:25:21 -0300

    Agregada rama developer

--- ProyectoFinal/src/main/java/com/proyectofinal/demo/ProyectoFinalEquipoAApplication.java
+++ ProyectoFinal/src/main/java/com/proyectofinal/demo/ProyectoFinalEquipoAApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProyectoFinalEquipoAApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProyectoFinalEquipoAApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoFinalEquipoAApplication.class, args);
+    }
 
 }
