f97705f971e8dc18409d2bd61a8013a2d4018761
Jeremias Rivelli <jeremias.j.riv@gmail.com>
2023-11-02 21:17:09 -0300

    Oferta arreglada

--- ProyectoFinal/src/main/java/com/proyectofinal/entidades/Oferta.java
+++ ProyectoFinal/src/main/java/com/proyectofinal/entidades/Oferta.java
@@ -1,7 +1,7 @@
 package com.proyectofinal.entidades;
 
 import java.io.Serializable;
-import java.time.LocalDate;
+import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -20,9 +20,9 @@ public class Oferta implements Serializable {
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String idOferta;
     private Integer valorOferta;
-    private LocalDate fechaOferta;
-    private LocalDate fechaAceptacion;
-    private LocalDate fechaRevocacion;
+    private Date fechaOferta;
+    private Date fechaAceptacion;
+    private Date fechaRevocacion;
     private String estadoOferta;
 
     @ManyToOne
