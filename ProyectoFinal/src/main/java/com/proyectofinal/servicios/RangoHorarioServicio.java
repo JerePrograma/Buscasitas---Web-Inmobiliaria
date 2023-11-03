fa523a6978b7667698f32dedd7df0ddb44c44bd3
Dynezel <dylan50xd@gmail.com>
2023-11-03 20:44:26 -0300

    cambios, no mergear todavia

--- ProyectoFinal/src/main/java/com/proyectofinal/servicios/RangoHorarioServicio.java
+++ ProyectoFinal/src/main/java/com/proyectofinal/servicios/RangoHorarioServicio.java
@@ -47,23 +47,26 @@ public class RangoHorarioServicio {
     }
 
     @Transactional
-    public RangoHorario actualizarRangoHorario(RangoHorario rangoHorario, LocalTime horaInicio, LocalTime horaFin) {
+    public RangoHorario actualizarRangoHorario(RangoHorario rangoHorario, List<String> diaSemanaList, List<String> horaInicioList, List<String> horaFinList) {
         if (rangoHorario != null) {
-            rangoHorario.setHoraInicio(horaInicio);
-            rangoHorario.setHoraFin(horaFin);
-            return rangoHorarioRepositorio.save(rangoHorario);
+            List<RangoHorario> rangosHorarios = new ArrayList<>();
+            for (int i = 0; i < diaSemanaList.size(); i++) {
+                LocalTime horaInicio = LocalTime.parse(horaInicioList.get(i));
+                LocalTime horaFin = LocalTime.parse(horaFinList.get(i));
+                rangoHorario.setDiaSemana(String.valueOf(diaSemanaList));
+                rangoHorario.setHoraInicio(horaInicio);
+                rangoHorario.setHoraFin(horaFin);
+                return rangoHorarioRepositorio.save(rangoHorario);
+            }
         }
         throw new IllegalArgumentException("El rango horario proporcionado es nulo.");
     }
 
     @Transactional
-    public void eliminarRangoHorario(Long id) throws Exception {
-        Optional<RangoHorario> rangoHorarioOptional = rangoHorarioRepositorio.findById(id);
-        if (rangoHorarioOptional.isPresent()) {
-            rangoHorarioRepositorio.delete(rangoHorarioOptional.get());
-        } else {
-            throw new Exception("No se encontró el rango horario con ID: " + id);
-        }
+    public void eliminarInmueblePorCuentaTributaria(String cuentaTributaria) {
+        // Implementa la lógica para eliminar un inmueble por su cuenta tributaria
+        List<RangoHorario> rangosHorarios = (List<RangoHorario>) rangoHorarioRepositorio.findByCuentaTributaria(cuentaTributaria);
+        rangoHorarioRepositorio.deleteAll(rangosHorarios);
     }
 
     public RangoHorario obtenerRangoHorarioPorId(Long id) throws Exception {
@@ -72,6 +75,13 @@ public class RangoHorarioServicio {
             return rangoHorarioOptional.get();
         }
         throw new Exception("No se encontró el rango horario con ID: " + id);
+    }
+    public RangoHorario obtenerRangoHorarioPorCuentaTributaria(String cuentaTributaria) throws Exception {
+       Optional<RangoHorario> rangoHorarioOptional = Optional.ofNullable(rangoHorarioRepositorio.findByCuentaTributaria(cuentaTributaria));
+        if (rangoHorarioOptional.isPresent()) {
+            return rangoHorarioOptional.get();
+        }
+        throw new Exception("No se encontró el rango horario con Cuenta Tributaria: " + cuentaTributaria);
     }
 
     public List<RangoHorario> obtenerTodosLosRangosHorarios() {
