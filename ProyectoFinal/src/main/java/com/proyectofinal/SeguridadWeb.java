acd05a08a5a8d4b1dbd45ed796660e6862d11f61
Jeremias Rivelli <jeremias.j.riv@gmail.com>
2023-11-03 22:30:22 -0300

    Manejo de roles en thymeleaf

--- ProyectoFinal/src/main/java/com/proyectofinal/SeguridadWeb.java
+++ ProyectoFinal/src/main/java/com/proyectofinal/SeguridadWeb.java
@@ -3,11 +3,14 @@ package com.proyectofinal;
 import com.proyectofinal.servicios.UsuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -45,5 +48,10 @@ public class SeguridadWeb extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and().csrf()
                 .disable();
+    }
+
+    public static boolean isAuthenticated() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication != null && !(authentication instanceof AnonymousAuthenticationToken) && authentication.isAuthenticated();
     }
 }
--- ProyectoFinal/src/main/java/com/proyectofinal/repositorios/OfertaRepositorio.java
+++ ProyectoFinal/src/main/java/com/proyectofinal/repositorios/OfertaRepositorio.java
@@ -1,18 +1,13 @@
-
 package com.proyectofinal.repositorios;
 
 import com.proyectofinal.entidades.Oferta;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OfertaRepositorio extends JpaRepository<Oferta, String> {
 
-     //buscar oferta por inmueble
-   // @Query("SELECT o FROM Oferta o WHERE o.oferta =  :oferta")
+    //buscar oferta por inmueble
+    // @Query("SELECT o FROM Oferta o WHERE o.oferta =  :oferta")
     //public Oferta buscarPorOferta(@Param("oferta") String oferta);
-
 }
