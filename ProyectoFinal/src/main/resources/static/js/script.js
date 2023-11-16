10464eab14aaf46376cf9e1bb6f860f9b2a39528
Dynezel <dylan50xd@gmail.com>
2023-11-16 20:05:26 -0300

    Cambios a cita

--- ProyectoFinal/src/main/resources/static/js/script.js
+++ ProyectoFinal/src/main/resources/static/js/script.js
@@ -14,14 +14,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const nuevoRangoHorario = document.createElement("div");
     nuevoRangoHorario.classList.add("rango-horario"); // Añadir una clase para el estilo si es necesario
     nuevoRangoHorario.innerHTML = `
-      <div class="form-group">
+            <div class="form-group">
+                <label>Fecha:</label>
+                <input type="date" name="fecha" class="fecha controls" required />
+            </div>
           <label>Día de la semana:</label>
-          <select name="diaSemana" class="diaSemana">
+          <select name="diaSemana" class="diaSemana controls">
               <option value="Lunes">Lunes</option>
               <option value="Martes">Martes</option>
               <option value="Miércoles">Miércoles</option>
               <option value="Jueves">Jueves</option>
               <option value="Viernes">Viernes</option>
+              <option value="Sabado">Sabado</option>
+              <option value="Domingo">Domingo</option>
           </select>
       </div>
       <div class="form-group">
@@ -35,6 +40,7 @@ document.addEventListener("DOMContentLoaded", function () {
     `;
 
     // Poblamos las opciones de tiempo para los selects de este nuevo rango horario
+    populateDateOptions(nuevoRangoHorario.querySelector(".fecha"));
     populateTimeOptions(nuevoRangoHorario.querySelector(".horaInicio"));
     populateTimeOptions(nuevoRangoHorario.querySelector(".horaFin"));
 
@@ -51,6 +57,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
   var startTime = document.getElementById("horaInicio");
   var endTime = document.getElementById("horaFin");
+
+    function populateDateOptions(input) {
+        // Puedes personalizar las opciones de fecha según tus necesidades
+        var today = new Date();
+        var options = "";
+        for (var i = 0; i < 7; i++) {
+          var date = new Date(today);
+          date.setDate(today.getDate() + i);
+          var formattedDate = date.toISOString().split("T")[0];
+          options += '<option value="' + formattedDate + '">' + formattedDate + "</option>";
+        }
+        input.innerHTML = options;
+      }
 
   function populateTimeOptions(input) {
     var options = "";
